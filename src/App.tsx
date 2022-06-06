d5eb898403c8e8eba0ceab9d1ffcdc0e36be3446
Sergii Tsekhmister <tsehmistersergey@gmail.com>
2022-06-06 20:06:18 +0300

    added input and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
-function App() {
+import InputField from './components/InputField';
+ 
+const App: React.FC = () => {
+  const [todo, setTodo] = useState('');
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className='heading'>Taskinet</span>
+      <InputField />
     </div>
   );
 }
